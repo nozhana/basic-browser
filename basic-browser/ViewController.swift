8c75e3516f3005925b7a9daf3f4714027b62db12
Nozhan Amiri <nozhana@gmail.com>
2022-04-24 20:48:29 +0430

    Initial Commit

---
+++ basic-browser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  basic-browser
+//
+//  Created by Nozhan Amiri on 4/24/22.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
