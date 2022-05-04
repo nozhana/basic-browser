0bf82bc1b7279730cc89188fd41c3ff70d255874
Nozhan Amiri <nozhana@gmail.com>
2022-05-04 15:27:47 +0430

    Add buttons
    Implement back/forward buttons
    Show toolbar
    Stub open page... button on toolbar

--- basic-browser/ViewController.swift
+++ basic-browser/ViewController.swift
@@ -19,6 +19,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.navigationDelegate = self
         view = webView
     }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: webView, action: #selector(webView.goBack))
+        backButton.tintColor = .blue
+        
+        let forwardButton = UIBarButtonItem(image: UIImage(systemName: "chevron.right"), style: .plain, target: webView, action: #selector(webView.goForward))
+        
+        let openButton = UIBarButtonItem(title: "Open page...", style: .plain, target: self, action: nil)
+        
+        if webView.canGoBack {
+            navigationItem.setLeftBarButton(backButton, animated: true)
+        } else {
+            navigationItem.setLeftBarButton(nil, animated: true)
+        }
+        
+        if webView.canGoForward {
+            navigationItem.setRightBarButton(forwardButton, animated: true)
+        } else {
+            navigationItem.setRightBarButton(nil, animated: true)
+        }
+        
+        toolbarItems = [openButton]
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
