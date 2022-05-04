3dbb59231381a08b06f49484690155d75478999c
Nozhan Amiri <nozhana@gmail.com>
2022-05-04 16:15:49 +0430

    Implement openButton
    Move openButton to viewDidLoad()
    Create action sheet for openButton
    Implement openTapped & openPage

--- basic-browser/ViewController.swift
+++ basic-browser/ViewController.swift
@@ -22,11 +22,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         let backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: webView, action: #selector(webView.goBack))
-        backButton.tintColor = .blue
         
         let forwardButton = UIBarButtonItem(image: UIImage(systemName: "chevron.right"), style: .plain, target: webView, action: #selector(webView.goForward))
-        
-        let openButton = UIBarButtonItem(title: "Open page...", style: .plain, target: self, action: nil)
         
         if webView.canGoBack {
             navigationItem.setLeftBarButton(backButton, animated: true)
@@ -39,9 +36,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
         } else {
             navigationItem.setRightBarButton(nil, animated: true)
         }
-        
-        toolbarItems = [openButton]
-        
+ 
+ 
     }
 
     override func viewDidLoad() {
@@ -52,9 +48,30 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+               
+        let openButton = UIBarButtonItem(title: "Open page", style: .plain, target: self, action: #selector(openTapped))
+               
+        toolbarItems = [openButton]
         
     }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "finnhub.io", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        ac.popoverPresentationController?.barButtonItem = toolbarItems?.first
+        present(ac, animated: true)
+    }
 
+    func openPage(alertAction action: UIAlertAction) {
+        let url = URL(string: "https://\(action.title!)")!
+        webView.load(URLRequest(url: url))
+    }
 
 }
 
